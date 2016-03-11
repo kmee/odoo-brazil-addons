96d89ebf0e9655cab5234aaf345597065df2f9f4
Luis Felipe Mileo <mileo@kmee.com.br>
2016-03-11 00:05:05 -0300

    [REM] View

--- l10n_br_product_variant_fiscal_multicompany/__openerp__.py
+++ l10n_br_product_variant_fiscal_multicompany/__openerp__.py
@@ -30,10 +30,7 @@
     'depends': [
         'l10n_br_product_ncm',
     ],
-    'data': [
-      'views/view.xml'
-    ],
     'installable': True,
     'auto_install': False,
-    'pre_init_hook': 'pre_init_hook',
+    #'pre_init_hook': 'pre_init_hook',
 }
